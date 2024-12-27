5ac45d51451c73c0ca3c8c480552ae1a14354d7a
Vladislav Luchnikov <whatislov@mail.ru>
2024-12-27 23:23:02 +0300

    feat: add cart endpoints

--- src/cart/views.py
+++ src/cart/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import CartItem
+from .serializers import CartItemSerializer
+
+
+class CartItemViewSet(viewsets.ModelViewSet):
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticated]
+    serializer_class = CartItemSerializer
+
+    def get_cart(self):
+        return self.request.user.cart
+
+    def get_queryset(self):
+        return CartItem.objects.filter(cart=self.get_cart())
+
+    def perform_create(self, serializer):
+        serializer.save(cart=self.get_cart())
