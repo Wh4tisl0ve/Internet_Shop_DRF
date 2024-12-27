c5666f47741e0c24fc6a07fb8325bcb5b7729df0
Vladislav Luchnikov <whatislov@mail.ru>
2024-12-27 21:25:35 +0300

    refactor: update urls path for products endpoints

--- src/products/urls.py
+++ src/products/urls.py
@@ -2,9 +2,16 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ProductsViewSet
 
+
 router = DefaultRouter()
 router.register(r"products", ProductsViewSet, basename="products")
 
+
 urlpatterns = [
     path("", include(router.urls)),
+    path(
+        "product/<int:id>/",
+        ProductsViewSet.as_view({"get": "retrieve"}),
+        name="product_by_id",
+    ),
 ]
