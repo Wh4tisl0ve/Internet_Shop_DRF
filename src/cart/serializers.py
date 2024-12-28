bc7e8b8faed6fe2880c41d73b3eafab1315824ef
Vladislav Luchnikov <whatislov@mail.ru>
2024-12-28 13:31:39 +0300

    refactor: update serializer in cart app

--- src/cart/serializers.py
+++ src/cart/serializers.py
@@ -2,11 +2,10 @@ from rest_framework import serializers
 from .models import Cart, CartItem
 
 
-
 class CartItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = CartItem
-        fields = "__all__"
+        fields = ("id", "quantity", "product")
 
 
 class CartSerializer(serializers.ModelSerializer):
